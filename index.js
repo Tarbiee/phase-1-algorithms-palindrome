c1ffbc4bf6d979f218a3128ee5c34000ed0fba41
Tarbiee <dorcusstaby@gmail.com>
2023-10-05 11:35:48 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  return word === word.split('').reverse().join('');
 }
 
 /* 
   Add your pseudocode here
+  First take the word which is of a string datatype
+  Split the word so that you can get each character of the word as a string
+  Reverse the splited chaacters
+  Then join them to form the reversed word 
+  Check for equality between the original word and splitted word
+  Return true if they are equal else return false
 */
 
 /*
